323632259e6a26b36a594010140a66fbaf4c1cbd
Shreyansh Jain <60681213+shreyanshjain11@users.noreply.github.com>
2020-08-14 18:31:33 +0530

    Add files via upload

---
+++ Technocolabs Internship/javascript tasks/task1.js
@@ -0,0 +1,11 @@
+const kelvin=293;//this stores temperature in kelvin.
+var celsius=kelvin-273;//this stores temperature in celsius.
+var fahrenheit=celsius*(9/5)+32;//this stores temperature in fahrenheit.
+fahrenheit=Math.round(fahrenheit);//this stores round of value of temperature in fahrenheit.
+console.log("The temperature is "+fahrenheit+" degrees Fahrenheit.");
+newton=celsius*(33/100);
+newton=Math.floor(newton);
+console.log("The temperature is "+newton+" degrees Newton.");
+
+
+
